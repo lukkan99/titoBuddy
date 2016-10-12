af9b6654d0b79126fa9b807572a08fe3f63344af
Tyler Lafayette <tylerlafayette@gmail.com>
2016-10-12 15:08:15 -0700

    Adding all files from v1.0

---
+++ titoBuddy/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace titoBuddy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chooseAction = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.chooseAction.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(20, -19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(252, 314);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // chooseAction
+            // 
+            this.chooseAction.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.chooseAction.Margin = new System.Windows.Forms.Padding(0, 20, 0, 0);
+            this.chooseAction.Name = "chooseAction";
+            this.chooseAction.Size = new System.Drawing.Size(208, 48);
+            this.chooseAction.Opening += new System.ComponentModel.CancelEventHandler(this.chooseAction_Opening);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(207, 22);
+            this.toolStripMenuItem1.Text = "Sing the Nutshack theme";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(207, 22);
+            this.toolStripMenuItem2.Text = "become a scammer";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(20, -19);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(252, 314);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Font = new System.Drawing.Font("Myriad Arabic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(278, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 48);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hello!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1049, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.MenuHighlight;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.chooseAction.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip chooseAction;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ titoBuddy/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.Media;
+using System.IO;
+
+namespace titoBuddy
+{
+    public struct Say
+    {
+        public string Text { get; set; }
+        public Say(string text)
+        {
+            Text = text;
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Thread dickTalk;
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Top = Screen.PrimaryScreen.Bounds.Height - this.Height - 40;
+            this.Left = Cursor.Position.X;
+            loop(sender, e);
+            quote(sender, e);
+        }
+        public void SpeakText(string text)
+        {
+            dickTalk = new Thread(new ParameterizedThreadStart(o => Synth(((string)o))));
+            dickTalk.Start(text);
+            label1.Text = text;
+        }
+        public void Synth(string whatToSay)
+        {
+            using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            {
+
+                // Configure the audio output. 
+                synth.SetOutputToDefaultAudioDevice();
+
+                // Speak a string synchronously
+                synth.Speak(whatToSay);
+            }
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            SpeakText("What do you want from me?");
+            chooseAction.Show(Cursor.Position);
+        }
+
+        private void chooseAction_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            SpeakText("It\u2019s the Nutshack! (Yee, yee)\r\nIt\u2019s the Nutshack! (What he say?)\r\nIt\u2019s the Nutshack! (Oh, yes! Yeeeah)\r\nIt\u2019s the Nutshack! (Hey, I got the Nutshack!)\r\nIt\u2019s the Nutshack! (\u2018The fuck you say, boyee?)\r\nIt\u2019s the Nutshack! (It\u2019s the Nutshack!)\r\nIt\u2019s the Nutshack! (It\u2019s the Nutshack!)\r\nIt\u2019s the Nutshack! (It\u2019s the Nutshack\u2026)\r\nIt\u2019s the Nutshack! (Hey!)\r\nIt\u2019s the Nutshack! (Piece a\u2019 nuts!)\r\nIt\u2019s the Nutshack! (Whoooa!)\r\nIt\u2019s the Nutshack!\r\nHoo-ugh!\r\nPhil\u2019s from the stone, Jack\u2019s from the pier,\r\nHoratio or Horat so beer!\r\nTito Dick \u201CDickman\u201D, baby!\r\nHe hates Phil and loves the ladies.\r\nJack\u2019s cool-ass lazy, he\u2019s still learnin\u2019.\r\nNumber one Cherry Pie, still a virgin.\r\nChita, meet da freak of da weekah!\r\nPhil\u2019s homegirl that Jack wanna keep her,\r\nBut that\u2019s not happenin\u2019, either!\r\nShakin\u2019 like a seizure, hold up, boys\r\nand spark this, take a breather.\r\nWith that reefer in my lungs,\r\nI got grapes, what you watchin\u2019, son?\r\nIt\u2019s the Nutshack! (Yah!)\r\nIt\u2019s the Nutshack! (AAAAARRRGH!)\r\nIt\u2019s the Nutshack!\r\nIt\u2019s the Nutshack!");
+        }
+
+        public async void loop(object sender, EventArgs e)
+        {
+            for(int x = 0; x < 1; x--)
+            {
+                this.Left = Cursor.Position.X - 150;
+                //= Cursor.Position.X;
+                await Task.Delay(10);
+            }
+        }
+
+        private async void quote(object sender, EventArgs e)
+        {
+            while(true)
+            {
+                Random random = new Random();
+                var names = new List<string> { "Horat or Horatio so beer!", "Your PC has a wirus call 1 800 Madarchod", "I am the strongest.", "I hate phil!", "OwO what\'s this?" };
+
+                int index = random.Next(names.Count);
+                var name = names[index];
+                names.RemoveAt(index);
+
+                SpeakText(name);
+                await Task.Delay(60000);
+            }
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox1.Visible = false;
+            Stream str = Properties.Resources.the;
+            SoundPlayer simpleSound = new SoundPlayer(str);
+            simpleSound.Play();
+            label1.Text = "Thank you for calling Vindaloos technical suppoet!";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("you can not kill tito dicke!");
+            e.Cancel = true;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+            pictureBox1.Visible = true;
+            SpeakText("I am tito dick again!");
+        }
+    }
+}
